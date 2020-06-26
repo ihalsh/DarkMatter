2260ead1433468bd88cedacff891952545cf0b83
Igor <igor.sheremet@gmail.com>
2020-06-26 15:04:00 +0300

    Ktx-app and Ktx-log

--- core/src/main/kotlin/com/github/ihalsh/darkmatter/DarkMatter.kt
+++ core/src/main/kotlin/com/github/ihalsh/darkmatter/DarkMatter.kt
@@ -1,69 +1,25 @@
 package com.github.ihalsh.darkmatter
 
+import com.badlogic.gdx.Application.LOG_DEBUG
 import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.Container
-import com.badlogic.gdx.scenes.scene2d.ui.Image
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
-import com.badlogic.gdx.utils.Scaling
-import com.badlogic.gdx.utils.viewport.ExtendViewport
-import kotlinx.coroutines.launch
-import ktx.app.KtxApplicationAdapter
-import ktx.assets.async.AssetStorage
-import ktx.async.KtxAsync
+import com.badlogic.gdx.Gdx.*
+import com.github.ihalsh.darkmatter.screens.DarkMatterScreen
+import ktx.app.KtxGame
+import ktx.app.KtxScreen
+import com.github.ihalsh.darkmatter.screens.FirstScreen
+import com.github.ihalsh.darkmatter.screens.SecondScreen
+import ktx.log.debug
+import ktx.log.logger
 
-class DarkMatter : KtxApplicationAdapter {
+private val LOG = logger<DarkMatter>()
 
-    private val stage: Stage by lazy { Stage(ExtendViewport(940f, 540f)) }
-    private val assetStorage: AssetStorage by lazy { AssetStorage() }
-
-    companion object {
-        const val IMAGE = "libgdx.png"
-    }
-
+class DarkMatter : KtxGame<DarkMatterScreen>() {
     override fun create() {
-
-        // ktx-async module requires initiating Kotlin coroutines context:
-        KtxAsync.initiate()
-
-        // Launching asynchronous coroutine that will _not_ block the rendering thread:
-        KtxAsync.launch {
-            assetStorage.apply {
-                // Loading assets. Notice the immediate returns.
-                // The coroutine will suspend until each asset is loaded:
-                val image = load<Texture>(IMAGE)
-                // Assets are loaded and we already have references to all of them:
-//                goToNextView(logo, bundle, skin)
-                addActorsToStage(TextureRegionDrawable(image))
-            }
-        }
-    }
-
-    private fun addActorsToStage(drawable: TextureRegionDrawable) {
-        // Do the stage layout
-        val image = Image(drawable, Scaling.fit)
-        stage.addActor(Container(image).apply {
-            size(600f, 100f)
-            setFillParent(true)
-        })
-    }
-
-    override fun render() {
-        stage.act()
-
-        Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        stage.draw()
-    }
-
-    override fun resize(width: Int, height: Int) {
-        stage.viewport.update(width, height, true)
-    }
-
-    override fun dispose() {
-        stage.dispose()
-        assetStorage.dispose()
+        app.logLevel = LOG_DEBUG
+        LOG.debug { "Game is created" }
+        addScreen(FirstScreen(this))
+        addScreen(SecondScreen(this))
+        setScreen<FirstScreen>()
     }
 }
+
---
+++ core/src/main/kotlin/com/github/ihalsh/darkmatter/screens/DarkMatterScreen.kt
@@ -0,0 +1,6 @@
+package com.github.ihalsh.darkmatter.screens
+
+import com.github.ihalsh.darkmatter.DarkMatter
+import ktx.app.KtxScreen
+
+abstract class DarkMatterScreen (val game: DarkMatter) : KtxScreen
